4ec6552a0ec141e9202d368777dd449caeaeebc5
TomHanksInAHadronCollider <twoodruff256@gmail.com>
2026-04-14 18:21:50 -0400

    changed for new assignment due 4/14/2026

--- ProductsAppFinal/ProductsAppFinal/Controllers/ProductsController.cs
+++ ProductsAppFinal/ProductsAppFinal/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "potato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-oy", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "nail", Category = "Hardware", Price = 16.99M }
         };
 
         public ProductsController()
